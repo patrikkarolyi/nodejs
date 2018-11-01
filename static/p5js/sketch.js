2a2403ae3c5b85ee89d5691cd96e270634d3b5b3
patrikkarolyi <patrikkarolyi@hotmail.com>
2018-11-01 11:33:36 +0100

    text and proper apperiance

--- static/p5js/sketch.js
+++ static/p5js/sketch.js
@@ -1,92 +1,103 @@
 vertices = [{
-	x: 100,
-	y: 200,
-	overBox: false,
-	locked: false,
-	id: 0
-},
-{
-	x: 300,
-	y: 200,
-	overBox: false,
-	locked: false,
-	id: 1
-}
+		x: 100,
+		y: 200,
+		overBox: false,
+		locked: false,
+		id: 0
+	},
+	{
+		x: 300,
+		y: 200,
+		overBox: false,
+		locked: false,
+		id: 1
+	},
+	{
+		x: 200,
+		y: 300,
+		overBox: false,
+		locked: false,
+		id: "ez egy kurvahosszu szoveg aminek ki kell fernie !!!!!"
+	}
 ]
 
 edges = [{
-a: vertices[0],
-b: vertices[1]
-}]
+		a: vertices[0],
+		b: vertices[1]
+	},
+	{
+		a: vertices[1],
+		b: vertices[2]
+	}
+]
 
 
-var boxSize = 20;
+var r = 50;
 var xOffset = 0.0;
 var yOffset = 0.0;
 var overSomething = false
 
 function setup() {
-createCanvas(800, 400);
-rectMode(RADIUS);
-strokeWeight(3);
+	createCanvas(800, 400);
+	strokeWeight(3);
+	textAlign(LEFT);
 }
 
 function draw() {
-background(255, 255, 200);
-overSomething = false;
-vertices.forEach(function(vertex) {
+	background(255, 255, 200);
+	overSomething = false;
 
-	if (mouseX > vertex.x - boxSize && mouseX < vertex.x + boxSize &&
-		mouseY > vertex.y - boxSize && mouseY < vertex.y + boxSize) {
-		if (!overSomething) {
-			vertex.overBox = true;
-			overSomething = true;
-		}
-		if (!vertex.locked) {
-			stroke(255, 156, 0);
-		}
-
-	} else {
+	edges.forEach(function(edge) {
 		stroke(0);
-		fill(0, 0, 255);
-		vertex.overBox = false;
-	}
-	rect(vertex.x, vertex.y, boxSize, boxSize);
-});
-edges.forEach(function(edge) {
-	stroke(0);
-	line(edge.a.x, edge.a.y, edge.b.x, edge.b.y);
-});
+		line(edge.a.x, edge.a.y, edge.b.x, edge.b.y);
+	});
 
+	vertices.forEach(function(vertex) {
+
+		var dx = vertex.x - mouseX;
+		var dy = vertex.y - mouseY;
+		if (dx * dx + dy * dy < r * r / 2) {
+			if (!overSomething) {
+				vertex.overBox = true;
+				overSomething = true;
+			}
+			text(vertex.id, 20, 20);
+			stroke(255, 156, 0);
+		} else {
+			stroke(0);
+			fill(30, 144, 255);
+			vertex.overBox = false;
+		}
+		ellipse(vertex.x, vertex.y, r, r);
+	});
 }
 
 function mousePressed() {
+	vertices.forEach(function(vertex) {
+		if (vertex.overBox) {
+			vertex.locked = true;
+			fill(255, 0, 0);
+			xOffset = mouseX - vertex.x;
+			yOffset = mouseY - vertex.y;
+			return;
+		} else {
+			vertex.locked = false;
+		}
 
-vertices.forEach(function(vertex) {
-	if (vertex.overBox) {
-		vertex.locked = true;
-		fill(255, 0, 0);
-		xOffset = mouseX - vertex.x;
-		yOffset = mouseY - vertex.y;
-		return;
-	} else {
-		vertex.locked = false;
-	}
-
-});
+	});
 }
 
 function mouseDragged() {
-vertices.forEach(function(vertex) {
-	if (vertex.locked) {
-		vertex.x = mouseX - xOffset;
-		vertex.y = mouseY - yOffset;
-	}
-});
+	vertices.forEach(function(vertex) {
+		if (vertex.locked) {
+			vertex.x = mouseX - xOffset;
+			vertex.y = mouseY - yOffset;
+		}
+	});
 }
 
 function mouseReleased() {
-vertices.forEach(function(vertex) {
-	vertex.locked = false;
-});
+	vertices.forEach(function(vertex) {
+		vertex.locked = false;
+	});
 }
