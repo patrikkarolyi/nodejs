e1f659d4ce9f7af40ca6a24e4c87a1a83d9939d8
patrikkarolyi <patrikkarolyi@hotmail.com>
2018-12-09 12:48:25 +0100

    getGraphMW test

---
+++ test/testtest.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+
+function osszead(a, b) {
+  return a + b;
+}
+
+it('should return 3 when a=1 and b=2', function () {
+  var c = osszead(1, 2);
+  expect(c).to.be.equal(3);
+});
+
